4327d150ca516294441d3e83bf51839c999e954a
lokyst11 <1814862199@qq.com>
2019-11-13 21:26:42 +0800

    创建axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import '../src/styles/index.less'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+// 设置axios常态地址
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios // 共享给所有的实例使用
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
