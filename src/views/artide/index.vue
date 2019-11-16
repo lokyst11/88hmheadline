94dcda33c5b06e4004b943ec390270548d80baa0
lokyst11 <1814862199@qq.com>
2019-11-16 20:36:41 +0800

    列表分页

--- src/views/artide/index.vue
+++ src/views/artide/index.vue
@@ -2,48 +2,131 @@
  <div class="artide">
    <!-- 筛选数据 -->
   <el-card class="box-card">
-  <div slot="header" class="clearfix">
-    <span>全部图文</span>
-  </div>
-  <!-- 卡片内容 -->
-  <el-form ref="form" :model="filterForm" label-width="80px">
-  <el-form-item label="文章状态">
+    <div slot="header" class="clearfix">
+      <span>全部图文</span>
+    </div>
+    <!-- 卡片内容 -->
+    <el-form ref="form" :model="filterForm" label-width="80px">
+    <el-form-item label="文章状态">
+      <el-form-item>
+      <el-radio-group v-model="filterForm.status">
+        <el-radio label="全部"></el-radio>
+        <el-radio label="草稿"></el-radio>
+        <el-radio label="待审核"></el-radio>
+        <el-radio label="审核通过"></el-radio>
+        <el-radio label="审核失败"></el-radio>
+      </el-radio-group>
+    </el-form-item>
+    </el-form-item>
+    <el-form-item label="频道列表">
+      <el-select placeholder="请选择活动区域" v-model="filterForm.channel_id">
+        <el-option label="区域一" value="shanghai"></el-option>
+        <el-option label="区域二" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="时间选择">
+      <el-date-picker
+        v-model="rangeDate"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期">
+      </el-date-picker>
+    </el-form-item>
     <el-form-item>
-    <el-radio-group v-model="filterForm.status">
-      <el-radio label="全部"></el-radio>
-      <el-radio label="草稿"></el-radio>
-      <el-radio label="待审核"></el-radio>
-      <el-radio label="审核通过"></el-radio>
-      <el-radio label="审核失败"></el-radio>
-    </el-radio-group>
-  </el-form-item>
-  </el-form-item>
-  <el-form-item label="频道列表">
-    <el-select placeholder="请选择活动区域" v-model="filterForm.channel_id">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="时间选择">
-    <el-date-picker
-      v-model="rangeDate"
-      type="daterange"
-      range-separator="至"
-      start-placeholder="开始日期"
-      end-placeholder="结束日期">
-    </el-date-picker>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary">立即查询</el-button>
-  </el-form-item>
-</el-form>
+      <el-button type="primary">立即查询</el-button>
+    </el-form-item>
+    </el-form>
 </el-card>
- </div>
+
+<!-- 文章列表 -->
+ <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>共找到{{totalCount}}条符合条件的内容</span>
+      </div>
+      <!--
+        el-table 表格组件
+        data 表格的数组，要求是数组
+        表格组件会使用 data 数据，在内部自己进行遍历，我们不需要自己写什么 v-for
+        你只需要告诉这个表格组件：
+          data 是啥
+          表头名是什么
+          列值是什么
+        el-table-column 表格列组件
+          prop 用来指定渲染哪个数据字段
+          label 表头名称
+          width 列宽
+
+        表格列默认只能渲染普通文本，如果想要渲染点儿别的东西，需要自定义表格列。
+       -->
+      <el-table
+        :data="articles"
+        style="width: 100%"
+        v-loading="loading">
+        <el-table-column
+          prop="date"
+          label="封面"
+          width="180">
+          <!--
+            自定义表格列
+            在 template 上声明 slot-scope="scope" ，然后就可以通过 scope.row 获取遍历项
+            scope.row 就相当于我们自己 v-for 的 item
+
+            注意：只有当你需要在自定义表格列模板中访问遍历项的时候才这么做
+           -->
+          <template slot-scope="scope">
+            <img width="50" :src="scope.row.cover.images[0]">
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="title"
+          label="标题"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="status"
+          label="状态">
+          <template slot-scope="scope">
+            <el-tag
+              :type="articleStatus[scope.row.status].type"
+            >{{ articleStatus[scope.row.status].label }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="pubdate"
+          label="发布日期">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="操作">
+          <template>
+            <el-button type="danger" size="mini">删除</el-button>
+            <el-button type="primary" size="mini">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+</el-card>
+
+<!-- 分页 -->
+<!-- 默认按照10条每页划分页码
+background：背景颜色
+pager：页码
+total：总数-->
+<el-pagination
+  background
+  layout="prev, pager, next"
+  :total="totalCount"
+  @current-change="onPageChange"
+  :disabled="loading">
+</el-pagination>
+
+</div>
 </template>
 
 <script>
 export default {
-  name: '',
+
+  name: 'articles',
   data () {
     return {
     // 过滤数据
@@ -53,7 +136,73 @@ export default {
         begin_pubdate: '',
         end_pubdate: ''
       },
-      rangeDate: ''
+      rangeDate: '',
+      articles: [],
+      articleStatus: [
+        {
+          type: '',
+          value: 0,
+          lable: '草稿'
+        }, {
+          type: 'warning',
+          value: 1,
+          lable: '待审核'
+        }, {
+          type: 'success',
+          value: 2,
+          lable: '审核通过'
+        }, {
+          type: 'danger',
+          value: 3,
+          lable: '审核失败'
+        }, {
+          type: 'info',
+          value: 4,
+          lable: '已删除'
+        }
+      ],
+      totalCount: 0, // 总记录数
+      loading: true
+    }
+  },
+
+  created () {
+    // 初始化时加载第一页数据
+    this.loadArticles()
+  },
+  methods: {
+    loadArticles (page = 1) {
+      this.loading = true
+      // 在我们的项目中除了登录的接口不需要token，其他所有的接口都需要提供token，否者返回401错误
+      const token = window.localStorage.getItem('user-token')
+      this.$axios({
+        method: 'GET',
+        url: '/articles',
+        headers: {
+        // 添加请求头
+        // 名字：后端要求把token放在请求头中使用，我们不能瞎写
+        // 注意格式要求Bearer后有个空格 用户token
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          page,
+          per_page: 10 // 默认10条每页
+        }
+      }).then(res => { // 成功
+        console.log(res)
+        // 更新文章列表数组
+        this.articles = res.data.data.results
+        // 更新总记录数
+        this.totalCount = res.data.data.total_count
+      }).catch(err => { // 失败
+        console.log(err, '获取数据失败')
+      }).finally(() => {
+        // 无论成功失败最终都要执行
+        this.loading = false
+      })
+    },
+    onPageChange (page) {
+      this.loadArticles(page)
     }
   }
 }
