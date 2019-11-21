18767d208820eca4ffefa50b111958e75a098b8b
lokyst11 <1814862199@qq.com>
2019-11-21 20:00:06 +0800

    账户信息完成，修改用户头像

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
-      <el-form ref="form" :model="user" label-width="80px">
+  <el-row>
+      <el-col :span="10">
+        <el-form ref="form" :model="user" label-width="80px">
         <el-form-item label="用户头像">
-            <el-upload
-                class="avatar-uploader"
-                action="https://jsonplaceholder.typicode.com/posts/"
-                :show-file-list="false"
-                >
-                <img width="100" :src="user.photo" class="avatar">
-                <!-- <i v-else class="el-icon-plus avatar-uploader-icon"></i> -->
-            </el-upload>
         </el-form-item>
         <el-form-item label="用户昵称">
             <el-input v-model="user.name"></el-input>
@@ -20,11 +14,29 @@
         <el-form-item label="用户邮箱">
             <el-input v-model="user.email"></el-input>
         </el-form-item>
+        <el-form-item label="手机号">
+            <el-input disabled v-model="user.mobile"></el-input>
+        </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="onSubmit">保存修改</el-button>
             <el-button>取消</el-button>
         </el-form-item>
       </el-form>
+    </el-col>
+    <el-col :offset="1" :span="5">
+      <!-- :http-request：自定义上传头像的请求行为 -->
+      <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :http-request="onUpload"
+          >
+          <img v-if="user.photo" width="100" :src="user.photo" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          <p>点击选择上传用户头像</p>
+      </el-upload>
+    </el-col>
+  </el-row>
   </div>
 </template>
 
@@ -51,7 +63,31 @@ export default {
   },
   methods: {
     onSubmit () {
+      // const name=this.user.name
+      // const email=this.user.email
+      // const intro=this.user.intro
+      const { name, email, intro } = this.user
       console.log('保存')
+      this.$axios({
+        method: 'PATCH',
+        url: '/user/profile',
+        data: {
+        // name: name,
+        // email: email,
+        // intro: intro
+          name,
+          email,
+          intro
+        }
+      }).then(res => {
+        this.$message({
+          type: 'success',
+          message: '修改成功'
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('修改失败')
+      })
     },
     loadUserProfile () {
       this.$axios({
@@ -63,6 +99,22 @@ export default {
       }).catch(err => {
         console.log(err)
         this.$message.error('获取失败')
+      })
+    },
+    // file:回调函数中接收到的一个参数，一个上传文件的配置对象
+    onUpload (config) {
+      const fd = new FormData()
+      fd.append('photo', config.file)
+      this.$axios({
+        method: 'PATCH',
+        url: '/user/photo',
+        data: fd
+      }).then(res => {
+        // 更新图片地址
+        this.user.photo = res.data.data.photo
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('上传失败')
       })
     }
   }
